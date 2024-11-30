d6c6dd5c684c0ccb5f3decae99ac23ac3964e857
KaranRawat21 <155565621+KaranRawat21@users.noreply.github.com>
2024-11-30 08:22:36 +0000

    isme humne variable ke baare me sikha.

---
+++ variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "Jaipur";
+let accountState;
+// agar aap javascript me variable declair 
+// karke chord dete hai aur kuch value assign nahi karte 
+// toh uski value undefined hoti hai.
+
+accountEmail = "karan@gmail.com"
+accountPassword = "karan@123";
+
+console.table([accountId, accountEmail, accountPassword, accountState]) //print output in a table form.
