96ce797ab908feef9306b826d0f0489c48f7a693
KaranRawat21 <155565621+KaranRawat21@users.noreply.github.com>
2024-11-30 09:05:46 +0000

    isme maine dataTypes ke baare me padha

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert('hello') // we are using Node.js, not browser
+
+
+//data types
+let name = "hitesh"; //string
+let age = 18; //number
+let isLoggedIn = false // boolean
+
+//number => 2 to power 53
+//bigint => for much bigger numbers
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => tto define uniqueness
+//object
+
+
+console.log(typeof undefined); //output is undefined
+console.log(typeof null); //output is object
