67821a6dc1521190838c87235acbcc3de998bb18
KaranRawat21 <155565621+KaranRawat21@users.noreply.github.com>
2024-12-01 18:37:24 +0000

    isme maine operations and concatination ke baare me padha.

---
+++ operations.js
@@ -0,0 +1,15 @@
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue)   
+
+let str1 = "karan";
+let str2 = " rawat";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log('1'+2+2);
+console.log(1+2+'1 ');
+console.log(3 +3 *2*2);
