148fec11a3f74a61385baa9253587bb89c7ee3e8
KaranRawat21 <155565621+KaranRawat21@users.noreply.github.com>
2024-12-02 16:49:53 +0000

    isme maine premitive and non-premitive data types samjha

---
+++ premitiveAndNonPrimitive.js
@@ -0,0 +1,13 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt.
+
+//JavaScript is a dynamic language, meaning that variables can hold
+// different types of data during runtime.
+
+const bigNumber = 3333334098789679670237493274037493750834n; //use to make bigint
+
+
+//Reference (Non Primitive)
+
+// Array, Objects, Functions
--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-console.log("This is test page!!");
+console.log("This is test page!!");  
